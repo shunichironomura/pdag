18cf3589b932c03ecbb17bbab16511e5792fa0ef
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-09-25 12:01:11 +0900

    mark the function attribute of Relationship as repr=False

--- src/xlrm/_static/_relationship.py
+++ src/xlrm/_static/_relationship.py
@@ -1,10 +1,10 @@
 from ._variable import VariableBase
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import Any, Callable
 
 
 @dataclass
 class Relationship:
-    function: Callable[..., Any]
+    function: Callable[..., Any] = field(repr=False)
     inputs: tuple[VariableBase[Any], ...]
     outputs: tuple[VariableBase[Any], ...]
