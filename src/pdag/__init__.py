e89a743ecbe68b621bd7385351596ef2ba7a3759
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-10-13 21:23:38 +0900

    add relationship decorator

--- src/pdag/__init__.py
+++ src/pdag/__init__.py
@@ -1,5 +1,6 @@
 """pdag: Parameter Directed Acyclic Graph."""
 
-__all__ = ["BooleanParameter", "NumericParameter", "Model", "Relationship"]
+__all__ = ["BooleanParameter", "NumericParameter", "Model", "Relationship", "relationship"]
+from ._decorator import relationship
 from ._model import Model, Relationship
 from ._parameter import BooleanParameter, NumericParameter
