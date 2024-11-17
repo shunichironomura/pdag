8d51d0903e29f711de06c20cd3ec80f67d4b278e
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-11-17 17:02:10 +0900

    Create ModelBase class

--- src/pdag/_node.py
+++ src/pdag/_node.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from ._model import ParameterBase, Relationship
+    from ._base import ParameterBase, Relationship
 
 
 class ParameterNode[T]:
