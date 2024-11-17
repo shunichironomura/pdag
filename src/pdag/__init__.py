6fd40e8074e07b56f1e5363f30beb1b1ddafba74
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-11-17 17:02:10 +0900

    Create ModelBase class

--- src/pdag/__init__.py
+++ src/pdag/__init__.py
@@ -1,6 +1,14 @@
 """pdag: Parameter Directed Acyclic Graph."""
 
-__all__ = ["BooleanParameter", "NumericParameter", "Model", "Relationship", "relationship", "CategoricalParameter"]
+__all__ = [
+    "BooleanParameter",
+    "NumericParameter",
+    "Model",
+    "Relationship",
+    "relationship",
+    "CategoricalParameter",
+]
+from ._base import Relationship
 from ._decorator import relationship
-from ._model import Model, Relationship
+from ._model import Model
 from ._parameter import BooleanParameter, CategoricalParameter, NumericParameter
