10b2d702771f833fe0fd02620e31292f616a2540
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-11-17 17:02:28 +0900

    Refactor umbrella model

--- examples/umbrella2.py
+++ examples/umbrella2.py
@@ -8,41 +8,46 @@ import pdag
 logger = logging.getLogger(__name__)
 
 
-def main() -> None:
-    with pdag.Model() as umbrella_model:
-        # External parameters (X)
-        is_raining = pdag.BooleanParameter("Is it raining?")
+with pdag.Model() as umbrella_model:
+    # External parameters (X)
+    is_raining = pdag.BooleanParameter("Is it raining?")
 
-        # Levers (L)
-        will_take_umbrella = pdag.BooleanParameter("Will I take an umbrella?")
-        will_take_travel_umbrella = pdag.BooleanParameter("Will I take a travel umbrella?")
+    # Levers (L)
+    will_take_umbrella = pdag.BooleanParameter("Will I take an umbrella?")
+    will_take_travel_umbrella = pdag.BooleanParameter("Will I take a travel umbrella?")
 
-        # Performance metrics (M)
-        wetness = pdag.NumericParameter("Wetness", unit=None, lower_bound=0, upper_bound=1)
-        convenience = pdag.NumericParameter("Convenience", unit=None, lower_bound=0, upper_bound=1)
+    # Performance metrics (M)
+    wetness = pdag.NumericParameter("Wetness", unit=None, lower_bound=0, upper_bound=1)
+    convenience = pdag.NumericParameter("Convenience", unit=None, lower_bound=0, upper_bound=1)
 
-        # Relationships (R)
-        @pdag.relationship((is_raining, will_take_umbrella, will_take_travel_umbrella), wetness)
-        def how_wet_will_i_get(is_raining: bool, will_take_umbrella: bool, will_take_travel_umbrella: bool) -> float:
-            if is_raining:
-                if will_take_umbrella:
-                    return 0
-                if will_take_travel_umbrella:
-                    return 0.5
-                return 1
-            return 0
-
-        @pdag.relationship((will_take_umbrella, will_take_travel_umbrella), convenience)
-        def how_convenient_will_it_be(will_take_umbrella: bool, will_take_travel_umbrella: bool) -> float:
-            convenience = 0.0
-
+    # Relationships (R)
+    @pdag.relationship((is_raining, will_take_umbrella, will_take_travel_umbrella), wetness)
+    def how_wet_will_i_get(is_raining: bool, will_take_umbrella: bool, will_take_travel_umbrella: bool) -> float:
+        if is_raining:
             if will_take_umbrella:
-                convenience -= 1.0
-
+                return 0
             if will_take_travel_umbrella:
-                convenience -= 0.5
+                return 0.5
+            return 1
+        return 0
 
-            return convenience
+    @pdag.relationship((will_take_umbrella, will_take_travel_umbrella), convenience)
+    def how_convenient_will_it_be(will_take_umbrella: bool, will_take_travel_umbrella: bool) -> float:
+        convenience = 0.0
+        if will_take_umbrella:
+            convenience -= 1.0
+        if will_take_travel_umbrella:
+            convenience -= 0.5
+        return convenience
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler(rich_tracebacks=True)],
+    )
 
     # # # Draw the graph
     # import matplotlib.pyplot as plt
@@ -73,17 +78,7 @@ def main() -> None:
     results = [umbrella_model.evaluate(input_) for input_ in inputs]  # type: ignore[arg-type] # TODO: Fix this type error
     logger.info(f"Results: {results}")
 
-    model = pdag.Model()
-    model.add_model(umbrella_model)
-    results2 = [model.evaluate(input_) for input_ in inputs]  # type: ignore[arg-type] # TODO: Fix this type error
-    logger.info(f"Results 2: {results2}")
-
-
-if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(message)s",
-        datefmt="[%X]",
-        handlers=[RichHandler(rich_tracebacks=True)],
-    )
-    main()
+    # model = pdag.Model()
+    # model.add_model(umbrella_model)
+    # results2 = [model.evaluate(input_) for input_ in inputs]  # type: ignore[arg-type] # TODO: Fix this type error
+    # logger.info(f"Results 2: {results2}")
