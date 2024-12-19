8cab270c00f41d768906639e665ae08e574c16e0
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2024-12-19 14:06:29 +0900

    add test code for react flow model

---
+++ tests/server/test_model.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from pdag._server._model import ReactFlowGraph
+
+
+def test_parse_react_flow_graph(datadir: Path) -> None:
+    react_flow_graph_json = (datadir / "react_flow_graph.json").read_text()
+    graph = ReactFlowGraph.model_validate_json(react_flow_graph_json)
+
+
+def test_dump_parsed_react_flow_graph(datadir: Path) -> None:
+    react_flow_graph_json = (datadir / "react_flow_graph.json").read_text()
+    graph = ReactFlowGraph.model_validate_json(react_flow_graph_json)
+    # Ignore the trailing newline differences
+    assert graph.model_dump_json(indent=4, by_alias=True, round_trip=True).rstrip() == react_flow_graph_json.rstrip()
