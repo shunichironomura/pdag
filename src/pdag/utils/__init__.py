6cdbc020473a2388437f66fef1bbd594b33295c1
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-03-06 00:47:00 +0900

    Remove nestedlist utils

--- src/pdag/utils/__init__.py
+++ src/pdag/utils/__init__.py
@@ -10,7 +10,6 @@ __all__ = [
     "merge_two_set_dicts",
     "multidef",
     "nested_list_from_mapping",
-    "nested_map",
     "topological_sort",
 ]
 
@@ -18,5 +17,4 @@ from ._ast_utils import get_function_body
 from ._dict_utils import merge_two_set_dicts
 from ._init_args_recorder import InitArgsRecorder
 from ._multidef import MultiDef, MultiDefMeta, MultiDefProtocol, multidef
-from ._nested_list import ElementOrArrayType, nested_list_from_mapping, nested_map
 from ._topological_sort import topological_sort
