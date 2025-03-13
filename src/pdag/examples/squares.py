d7de280423873a0090efbc2b38b5f8bfb6907a94
Shunichiro Nomura <nomura@space.t.u-tokyo.ac.jp>
2025-03-13 22:49:19 +0900

    Add squares model

---
+++ src/pdag/examples/squares.py
@@ -0,0 +1,63 @@
+"""Squares example to demonstrate mapping containing submodels."""
+
+from typing import Annotated
+
+import pdag
+
+
+class SquareModel(pdag.Model):
+    """Square model.
+
+    Calculates y = x^2
+    """
+
+    x = pdag.RealParameter(...)
+    y = pdag.RealParameter(...)
+
+    @pdag.relationship
+    @staticmethod
+    def square(*, x: Annotated[float, x.ref()]) -> Annotated[float, y.ref()]:  # noqa: D102
+        return x**2
+
+
+class PolynomialModel(pdag.Model):
+    """Polynomial model.
+
+    Calculates z = x^2 + y^2
+    """
+
+    x = pdag.RealParameter(...)
+    x_squared = pdag.RealParameter(...)
+    y = pdag.RealParameter(...)
+    y_squared = pdag.RealParameter(...)
+    z = pdag.RealParameter(...)
+
+    calc_square_term = pdag.Mapping(
+        ...,
+        {
+            input_param.name: SquareModel.to_relationship(
+                ...,
+                inputs={SquareModel.x.ref(): input_param.ref()},
+                outputs={SquareModel.y.ref(): output_param.ref()},
+            )
+            for (input_param, output_param) in [(x, x_squared), (y, y_squared)]
+        },
+    )
+
+    @pdag.relationship
+    @staticmethod
+    def squares(  # noqa: D102
+        *,
+        x_squared: Annotated[float, x_squared.ref()],
+        y_squared: Annotated[float, y_squared.ref()],
+    ) -> Annotated[float, z.ref()]:
+        return x_squared + y_squared
+
+
+if __name__ == "__main__":
+    from rich import print  # noqa: A004
+
+    core_model = PolynomialModel.to_core_model()
+    print(core_model)
+    exec_model = pdag.create_exec_model_from_core_model(core_model)
+    print(exec_model)
