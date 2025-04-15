b9b6e17921a93a9f0f8e5fab06ec63627cadd52a
Riki Nakamura <riki.nakamura.edu@gmail.com>
2025-04-15 15:49:06 +0900

    Fix bug in export_dot function

--- src/pdag/_export/dot.py
+++ src/pdag/_export/dot.py
@@ -73,20 +73,36 @@ def export_dot(core_model: CoreModel, path: Path) -> None:  # noqa: C901, PLR091
 
     for name, collection in core_model.collections.items():
         graph.add_node(_mapping_node(collection))
+
+        input_params = set()
+        output_params = set()
+
         for item in collection.values():
             if isinstance(item, RelationshipABC):
                 for input_ref in item.iter_input_refs():
                     if isinstance(input_ref, ExecInfo):
-                        if input_ref.attribute not in added_exec_infos:
-                            graph.add_node(pydot.Node(input_ref.attribute, shape="diamond"))
-                            added_exec_infos.add(input_ref.attribute)
-                        graph.add_edge(pydot.Edge(input_ref.attribute, name))
+                        input_params.add(("execinfo", input_ref.attribute))
                     else:
-                        style = "dashed" if input_ref.previous else "solid"
-                        graph.add_edge(pydot.Edge(input_ref.name, name, style=style))
+                        input_params.add(("param", input_ref.name, input_ref.previous))
 
                 for output_ref in item.iter_output_refs():
-                    style = "dashed" if output_ref.next else "solid"
-                    graph.add_edge(pydot.Edge(name, output_ref.name, style=style))
+                    output_params.add(("param", output_ref.name, output_ref.next))
+
+        for i in input_params:
+            if i[0] == "execinfo":
+                attr = i[1]
+                if attr not in added_exec_infos:
+                    graph.add_node(pydot.Node(attr, shape="diamond"))
+                    added_exec_infos.add(attr)
+                graph.add_edge(pydot.Edge(attr, name))
+            else:
+                param = i[1]
+                style = "dashed" if i[2] else "solid"
+                graph.add_edge(pydot.Edge(param, name, style=style))
+
+        for o in output_params:
+            param = o[1]
+            style = "dashed" if o[2] else "solid"
+            graph.add_edge(pydot.Edge(name, param, style=style))
 
     graph.write(path, format="png")
